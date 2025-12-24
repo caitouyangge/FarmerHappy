c74db334cb4e5d393a2c18dacdbc3d53230c88c4
caitouyangge <78825426@qq.com>
2025-12-24 19:17:00 +0800

    feat: 删去多余组件

--- farmer_happy/src/components/Navbar.vue
+++ farmer_happy/src/components/Navbar.vue
@@ -27,23 +27,6 @@
           <span class="item-text">个人信息</span>
         </li>
       </ul>
-
-      <div class="navbar-user">
-        <div class="user-avatar" @click="navigate('/profile')">
-          {{ userInitial }}
-        </div>
-        <div class="user-info">
-          <div class="user-name">{{ userInfo.nickname || '用户' }}</div>
-          <div class="user-role">{{ userRoleText }}</div>
-        </div>
-        <button class="btn-logout" @click="handleLogout">
-          <svg class="logout-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-            <polyline points="16 17 21 12 16 7"></polyline>
-            <line x1="21" y1="12" x2="9" y2="12"></line>
-          </svg>
-        </button>
-      </div>
     </div>
   </nav>
 </template>
